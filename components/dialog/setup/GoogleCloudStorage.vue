d8c4f93ae167af5c2cd62e1c2b4d5ffeb74fe5b3
Sai K K <saikksub@gmail.com>
2024-02-12 08:55:08 +0530

    Update dialog for gcp

--- components/dialog/setup/GoogleCloudStorage.vue
+++ components/dialog/setup/GoogleCloudStorage.vue
@@ -1,6 +1,6 @@
 <template>
-  <UModal v-model="isOpen">
-    <UCard>
+  <UModal v-model="isOpen" :preventClose="state.prep">
+    <UCard v-if="state.collect">
       <template #header>
         <div class="flex items-center gap-3 h-full">
           <p class="text-lg">
@@ -23,6 +23,15 @@
           Please upload your Google Service Account JSON file, stored safely on your computer, to establish the connection.
           Rest assured, this file will not be shared on the internet.
         </p>
+
+        <UButton
+          icon="i-heroicons-computer-desktop"
+          variant="outline"
+          size="lg"
+          @click="onClickSelectFile"
+        >
+          Select file from your computer
+        </UButton>
       </div>
 
       <template #footer>
@@ -33,15 +42,111 @@
         </div>
       </template>
     </UCard>
+    <UCard v-else-if="state.prep">
+      <template #header>
+        <div class="flex items-center gap-3 h-full">
+          <p class="text-lg">
+            Connected!
+          </p>
+          <div class="grow" />
+          <ui-name-icon identifier="gcp" class="w-6 h-auto mt-2" />
+        </div>
+      </template>
+
+      <UButton
+        variant="ghost"
+        icon="i-heroicons-check-circle"
+        block
+        color="white"
+        :ui="{ block: 'justify-start' }"
+      >
+        Testing Service Account
+      </UButton>
+
+      <UButton
+        variant="ghost"
+        icon="i-heroicons-check-circle"
+        block
+        color="white"
+        :ui="{ block: 'justify-start' }"
+      >
+        Create a sample file: asfkaslfjaslkdf/sample.txt
+      </UButton>
+
+      <UButton
+        variant="ghost"
+        icon="i-heroicons-check-circle"
+        block
+        color="white"
+        :ui="{ block: 'justify-start' }"
+      >
+        read the sample file: asfkaslfjaslkdf/sample.txt
+      </UButton>
+
+      <UButton
+        variant="ghost"
+        icon="i-heroicons-check-circle"
+        block
+        color="white"
+        :ui="{ block: 'justify-start' }"
+      >
+        Your connection is ready!
+      </UButton>
+
+      <template #footer>
+        <div class="flex items-center gap-3">
+          <div class="grow" />
+          <UButton
+            label="Finish"
+            @click="close"
+          />
+        </div>
+      </template>
+    </UCard>
   </UModal>
 </template>
 
 <script setup>
+import { open as selectFile } from '@tauri-apps/api/dialog';
+import { readTextFile } from '@tauri-apps/api/fs';
+
 const isOpen = ref(false)
-const refInputFile = ref(null)
+const state = ref({
+  collect: true,
+  prep: false
+})
 
 const open = function () {
   isOpen.value = true
+}
+
+const close = function () {
+  isOpen.value = false
+}
+
+const onClickSelectFile = async function () {
+  try {
+    const selected = await selectFile({
+      multiple: false,
+      filters: [{
+        name: 'Service Account',
+        extensions: ['json']
+      }]
+    });
+
+    if (!selected) {
+      return
+    }
+
+    console.log(selected)
+    const serviceAccount = JSON.parse(await readTextFile(selected) || '')
+
+    console.log(serviceAccount)
+    state.value.collect = false
+    state.value.prep = true
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 defineExpose({ open })
